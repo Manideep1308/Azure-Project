5fc3bad6a50850b7bf2645a705f67db1e093b11a
Manideep1308 <manideep.sankabathula@innoart.in>
2022-06-02 14:16:09 +0530

    no changes

--- shell.sh
+++ shell.sh
@@ -4,7 +4,7 @@ echo "logging to azure portal.."
 az login
 
 echo "creating resource group"
-az group create --name $1 --location eastus
+az group create --name $1 --location centralus
 
 echo "deploying ARM template"
 az deployment group create --resource-group $1 --template-file $2
