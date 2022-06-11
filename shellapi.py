e2d8bfadd1c037ad3ae800d3abdd939bae914205
Manideep1308 <manideep.sankabathula@innoart.in>
2022-06-11 16:51:10 +0530

    some

--- shellapi.py
+++ shellapi.py
@@ -2,7 +2,11 @@ import os
 from flask import Flask, jsonify, request
 
 
+from flask_cors import CORS
+ 
+ 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/name', methods=['POST'])
 def method():
@@ -11,7 +15,11 @@ def method():
    templatename = request.args.get('templatename')
    resource_group_name =request.args.get('resource_group_name')
    os.system('/bin/bash --rcfile sh shell.sh '+ str(resource_group_name) + ' ' + str(templatename))
-   return 'do deployement'
+   return (
+     '{\n'
+     '   "status" : 200 \n'
+     '}\n'
+   )
 
 if __name__=='__main__':   
     app.run(port=9000, host='0.0.0.0')
